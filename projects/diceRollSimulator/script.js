fc7e5e49f8b5a1b0eb3bb147491e8ea30de70a2d
Ravi Singh <ravisingh9990@gmail.com>
2025-03-26 23:44:58 +0530

    diceRollSimulator Project Done

---
+++ projects/diceRollSimulator/script.js
@@ -0,0 +1,14 @@
+const dice = document.getElementById('dice');
+const rollButton = document.getElementById('rollButton');
+const resultMessage = document.getElementById('resultMessage');
+
+const diceFaces = ["\u2680", "\u2681", "\u2682", "\u2683", "\u2684", "\u2685"];
+
+function rollDice(){
+    const randomIndex = Math.floor(Math.random() * 6);
+
+    dice.textContent = diceFaces[randomIndex];
+    resultMessage.textContent = `You rolled a ${randomIndex + 1}`;
+
+}
+rollButton.addEventListener('click', rollDice);
